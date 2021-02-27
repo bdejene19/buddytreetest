18da146ab7d7e54b3540fa3947625e16d439964b
Bemnet Dejene <bdejene19@gmail.com>
2021-02-27 12:23:30 -0500

    uploaded infinite scroll using @material-ui and intersection observer API for infinite scroll. Calling data from JSONplaceholder.com

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,107 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import BasicCard from '../components/BasicCard'
+import {useEffect, useState, useRef  } from 'react';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+/**
+ * assessment instructions:
+ * 
+ * "Please use Next.js
+ * utilize API Routes and rewrites where applicable
+ * use Material-UI. 
+ * Create an endless feed application where you can click on an item and go to a 
+ * generic info page within the application. (Please provide a link to your work within three days).
+ */
 
 export default function Home() {
+  let responseData = [];
+  const [data, setData] = useState(null);
+  const [addedContent, setAddedContent] = useState(null);
+  async function getAPIData() {
+    responseData = await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+    setData(responseData.map(data =>   <BasicCard key={data.id} name={data.name} username={data.username} city={data.address.city} zipcode={data.address.zipcode}></BasicCard>))
+
+    return data;
+  }
+
+  let loaderRef = useRef(null);
+
+  function loadMore(totalEntries, observer) {
+    totalEntries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.intersectionRatio >= 0.6 && addedContent !== null) {
+          document.getElementById('loadSpinner').style.display = 'inline-block';
+          getAPIData();
+          setAddedContent([...addedContent, data]);
+          document.getElementById('loadSpinner').style.display = 'none';
+
+
+        } 
+        if (entry.intersectionRatio >= 0.6 && addedContent === null) {
+          document.getElementById('loadSpinner').style.display = 'inline-block';
+          getAPIData();
+          setAddedContent(data);
+          document.getElementById('loadSpinner').style.display = 'none';
+
+        } 
+      }      
+    })
+  }
+  
+
+  useEffect(() => {
+    getAPIData()
+    
+  }, [])
+
+  useEffect(() => {
+    
+    let parentAncestorScroll = document.querySelector('#masterParent');
+
+    let observerOptions = {
+      root: parentAncestorScroll,
+      rootMargin: '50px',
+      //threshold 1.0 = callback when 100% of observer is in view
+      threshold: 0.9,
+    }
+  
+    let observer = new IntersectionObserver(loadMore, observerOptions);
+    
+    observer.observe(document.querySelector('#addedContent'))
+   
+
+   
+  }, [loaderRef, loadMore])
+  
+
+  // create intersection observer
+  
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      {/* my endless feed container */}
+      <div className={styles.endlessContainer} id='masterParent'>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.endlessDiv}>
+          {data}      
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div>
+          {addedContent}
+        </div>
+
+        <div id='addedContent' ref={loadingRef => loadingRef = loadingRef}>
+        </div>
+        
+      </div>
+      <LoadingSpinner className={styles.loadSpinner} id='loadSpinner'></LoadingSpinner>
+
+
     </div>
   )
 }
